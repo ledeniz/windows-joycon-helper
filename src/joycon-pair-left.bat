b6ad9455ad05c98f1060da8be6033d9d6be14ce8
Deniz Erdogan <github@ledeniz.de>
2023-01-25 18:28:41 +0100

    Add unpair command before pair command

--- src/joycon-pair-left.bat
+++ src/joycon-pair-left.bat
@@ -2,4 +2,5 @@
 echo "Hold the sync button of the left Joy-Con now"
 @echo on
 
+btpair -u -n "Joy-Con (L)"
 btpair -p -n "Joy-Con (L)"
--- src/joycon-pair-right.bat
+++ src/joycon-pair-right.bat
@@ -2,4 +2,5 @@
 echo "Hold the sync button of the right Joy-Con now"
 @echo on
 
+btpair -u -n "Joy-Con (R)"
 btpair -p -n "Joy-Con (R)"
